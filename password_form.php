03d383b08b75c114321b4e11664874b5115a1592
Ryan Rapini <ryanrapini@gmail.com>
2018-01-26 20:42:30 -0500

    better looking form

--- password_form.php
+++ password_form.php
@@ -1,16 +1,25 @@
 <?php 
-	$config = include('allowme_config.php');
+$config = include('allowme_config.php');
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title><?php $config['title'] ? $config['title'] : "php-allowme" ?></title>
+	<style>
+		.centered {
+		    margin: 0 auto;
+		    width: 200px;
+		}
+	</style>
 </head>
 <body>
-	<form method="POST" action="index.php">
-        Pass <input type="password" name="pass"></input><br/>
-        <input type="submit" name="submit" value="Go"></input>
-	</form>
+	<div class="centered">
+		<p>Enter the password to be granted access.</p>
+		<form method="POST" action="index.php">
+			Pass <input type="password" name="pass"></input><br/>
+			<input type="submit" name="submit" value="Submit"></input>
+		</form>
+	</div>
 </body>
 </html>
